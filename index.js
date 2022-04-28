95ff8fd9a98d2098ff1cd0d16e779059d703d1e4
Kenneth Mei <meikenneth321@gmail.com>
2022-04-28 10:12:02 -0400

    commit tree 2c8738990c333e93baf119b7498c2f44310e6bd5

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function getTheW(obj){
+    return obj.result === "W"
+  }
+function superbowlWin(array){
+    const data = array.find(getTheW);
+    if (data){
+        return data.year;
+    }
+  }
+  console.log(superbowlWin(record));
+
+  
